1414ec4aff7c6dd7c50f931c7819779ac3d1e9db
mickdevries <mickdevries@gmail.com>
2017-02-16 14:00:17 +0100

    -

--- WK1_HW1/footer.php
+++ WK1_HW1/footer.php
@@ -5,6 +5,7 @@
  * Date: 09-Feb-17
  * Time: 15:04
  */
+$date = date("d-m-y H:i");
 ?>
 <html lang="en">
 <head>
@@ -12,6 +13,6 @@
 </head>
 <footer>
     <meta charset="UTF-8">
-    <h3>Made by Mick and Victor</h3>
+    <h3>Made by Mick and Victor <?php echo $date ?></h3>
 </footer>
 </html>
