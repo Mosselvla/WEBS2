674d83013eb7cd22ab917a1d859292a6a809e1d8
Victor <you@example.com>
2017-02-18 18:27:56 +0100

    Making things nicer + detailed product page

---
+++ WK1_HW1/product.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 18-Feb-17
+ * Time: 18:25
+ */
+    include 'header.php';
+?>
+<html>
+    <head>
+        <link rel="stylesheet" href="Body.css">
+    </head>
+    <body>
+
+    </body>
+</html>
+<?php
+    include 'footer.php';
+?>
