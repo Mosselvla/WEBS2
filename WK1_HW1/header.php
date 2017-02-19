d110416cafc74de4ae2afacf831f2dd583e8a93f
Victor <you@example.com>
2017-02-19 12:38:40 +0100

    Making things nicer + detailed product page

--- WK1_HW1/header.php
+++ WK1_HW1/header.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="Header.css">
-    <title>Title</title>
+    <title>VMwares</title>
 
 </head>
 <body>
--- WK1_HW1/products.php
+++ WK1_HW1/products.php
@@ -13,7 +13,37 @@ include 'header.php';
     </head>
     <body background="background.png">
         <div>
-            ayylmao
+            <table>
+                <tbody>
+                    <tr style="background-color:#4CAF50:white;">
+                        <th>Productnaam</th>
+                        <th>Prijs</th>
+                        <th>Score</th>
+                        <th>Details</th>
+                    </tr>
+                    <tr>
+                        <td>Data Package 1</td>
+                        <td>€59,99</td>
+                        <td>80/100</td>
+                        <td><a href="">Link</a></td>
+
+                    </tr>
+                    <tr>
+                        <td>Data Package 2</td>
+                        <td>€89,99</td>
+                        <td>90/100</td>
+                        <td><a href="">Link</a></td>
+
+                    </tr>
+                    <tr>
+                        <td>Data Package 3</td>
+                        <td>€129,99</td>
+                        <td>100/100</td>
+                        <td><a href="">Link</a></td>
+
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </body>
 </html>
